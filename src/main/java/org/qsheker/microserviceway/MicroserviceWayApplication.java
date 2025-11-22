e3d54fad2653a2cb76082e6032ce4439332f16bc
Aldiyar <aldikzhaks@gmail.com>
2025-11-22 19:32:36 +0500

    initial commit

---
+++ src/main/java/org/qsheker/microserviceway/MicroserviceWayApplication.java
@@ -0,0 +1,13 @@
+package org.qsheker.microserviceway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/qsheker/microserviceway/MicroserviceWayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qsheker.microserviceway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
