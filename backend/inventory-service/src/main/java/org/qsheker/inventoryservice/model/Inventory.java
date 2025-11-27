a7bf84932c098e45ea46dc594c9d82dd8a111301
Aldiyar <aldikzhaks@gmail.com>
2025-11-27 17:40:55 +0500

    Init frontend

---
+++ backend/inventory-service/src/main/java/org/qsheker/inventoryservice/model/Inventory.java
@@ -0,0 +1,27 @@
+package org.qsheker.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "inventory")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sku_code")
+    private String skuCode;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    private Integer quantity;
+}
+
